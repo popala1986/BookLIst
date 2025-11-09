aae1ef96906e9d86cf62d45198c67d17f6953747
Paweł <p.opala1986@gmail.com>
2025-11-09 17:03:34 +0100

    feat(controller): add REST controller for books,

---
+++ src/main/java/pl/booklist/controller/BookController.java
@@ -0,0 +1,44 @@
+package pl.booklist.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.booklist.model.Book;
+import pl.booklist.service.BookService;
+
+import java.util.List;
+
+/**
+ * REST controller for handling book-related HTTP requests.
+ * Provides endpoints for retrieving book data.
+ * </p>
+ * This controller is validated and uses {@link BookService} to access book entities.
+ * </p>
+ *
+ * @author Paweł
+ */
+@RestController
+@Validated
+public class BookController {
+
+    private final BookService bookService;
+
+
+    /**
+     * Constructs a new {@code BookController} with the given {@link BookService}.
+     *
+     * @param bookService the service used to manage book entities.
+     */
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    /**
+     * Handles GET request to retrieve all books.
+     * @return a list of all {@link Book} entities from the database.
+     */
+    @GetMapping("/showAllBooks")
+    public List<Book> findAll() {
+        return bookService.findAllBooks();
+    }
+}
